87b144f42d7eabe3a1f32a8627170a40bfe5be0e
Václav Blažek <vaclav.blazek@citationtech.net>
2012-05-22 12:46:11 +0000

    shared/utility: added to repo, implemented utility::map function
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@892 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/detail/map.hpp
@@ -0,0 +1,93 @@
+#ifndef shared_utility_detail_map_hpp_included_
+#define shared_utility_detail_map_hpp_included_
+
+#include <cstdlib>
+#include <boost/range.hpp>
+#include <boost/format.hpp>
+
+namespace utility { namespace detail {
+
+template <typename CallResult>
+class Result {
+public:
+    void set(const std::exception_ptr &exc) {
+        exc_ = exc;
+        result_.reset();
+    }
+
+    void set(CallResult &&result) {
+        exc_ = 0x0;
+        result_ = result;
+    }
+
+    const CallResult& get() const {
+        if (result_) {
+            return *result_;
+        }
+        std::rethrow_exception(exc_);
+    }
+
+    CallResult& get() {
+        if (result_) {
+            return *result_;
+        }
+        std::rethrow_exception(exc_);
+    }
+
+    const CallResult& operator*() const { return get(); }
+
+    CallResult& operator*() { return get(); }
+
+    const CallResult* operator->() const { return &get(); }
+
+    CallResult* operator->() { return &get(); }
+
+    bool valid() const {
+        return result_ != boost::none;
+    }
+
+private:
+    std::exception_ptr exc_;
+    boost::optional<CallResult> result_;
+};
+
+template <typename Sequence, typename Callable, typename... Args>
+class map_helper {
+public:
+    typedef typename Sequence::value_type value_type;
+
+    typedef typename std::result_of
+        <Callable(const value_type&, Args...)>::type CallResult;
+
+    typedef detail::Result<CallResult> Result;
+    typedef std::vector<Result> ResultList;
+    typedef boost::sub_range<const Sequence> SequenceSubRange;
+
+    map_helper() {}
+
+    template <typename... Args2>
+    void operator()(const SequenceSubRange &values, const std::string &name
+                    , size_t index, size_t count
+                    , typename ResultList::iterator result
+                    , const Callable &callable, Args2&& ...args)
+        const
+    {
+        BOOST_FOREACH(const auto &value, values) {
+            dbglog::thread_id(str(boost::format("%s [%d/%d]")
+                                  % name % (index + 1) % count));
+
+            try {
+                result->set(callable(value, args...));
+            } catch(...) {
+                result->set(std::current_exception());
+            }
+
+            ++result;
+            ++index;
+        }
+    }
+};
+
+} } // namespace utility::detail
+
+#endif // shared_utility_detail_map_hpp_included_
---
+++ utility/map.hpp
@@ -0,0 +1,120 @@
+#ifndef shared_utility_map_hpp_included_
+#define shared_utility_map_hpp_included_
+
+#include <future>
+#include <cstdlib>
+
+#include <boost/thread.hpp>
+#include <boost/range.hpp>
+#include <boost/format.hpp>
+
+#include <utility/detail/map.hpp>
+
+namespace utility {
+
+/** Returns [ callable(value, args...) for value in values ], i.e. vector of
+ * values mapped by `callable'.
+ *
+ * Operations are performed in parallel if there are more than one execution
+ * unit (CPU, CPU core) available and there are more than one elements in
+ * `value'.
+ *
+ * Result vector value_type behaves almost like boost::optional<X> where X is
+ * result type of function call; you can use get(), operator* and operator->.
+ *
+ * If function call terminates with an exception this exception is saved and
+ * thrown on get/operator* /operator-> calls.
+ *
+ * \param name name of operation (used in log messages)
+ * \param values sequence of values to process
+ * \param callable callable(const Sequence::value_type&, args...)
+ * \param ...args arguments to be passed to each call of callable
+ * \return list of results
+ */
+template <typename Sequence, typename Callable, typename... Args>
+typename detail::map_helper<Sequence, Callable, Args...>::ResultList
+map(const std::string &name, const Sequence &values
+    , const Callable &callable, Args&& ...args);
+
+// ************************************************************************
+// Implementation
+
+template <typename Sequence, typename Callable, typename... Args>
+typename detail::map_helper<Sequence, Callable, Args...>::ResultList
+map(const std::string &name, const Sequence &values
+    , const Callable &callable, Args&& ...args)
+{
+    typedef detail::map_helper<Sequence, Callable, Args...> Helper;
+    typedef typename Helper::Result Result;
+    typedef typename Helper::ResultList ResultList;
+    typedef typename Helper::SequenceSubRange SequenceSubRange;
+
+    Helper helper;
+
+    // NB: std::thread::hardware_concurrency() returned 0 at the time when this
+    // was written, using boost::thread::hardware_concurrency() instead
+    size_t threadCount
+        (std::min<size_t>
+         (boost::thread::hardware_concurrency(), values.size()));
+
+    if (getenv("NO_THREADS")) {
+        threadCount = 1;
+    }
+
+    if (threadCount < 2) {
+        // single thread -> just run here
+        ResultList result;
+        result.resize(values.size());
+
+        helper(SequenceSubRange(values), name, 0, values.size()
+               , result.begin(), callable, args...);
+        return result;
+    }
+
+    std::vector<std::future<void> > jobs;
+    jobs.reserve(values.size());
+
+    ResultList result;
+    result.resize(values.size());
+
+    size_t count(values.size());
+
+    // compute partition size
+    size_t partition(count / threadCount);
+    int extra(count % threadCount);
+
+    // start thread logging
+    dbglog::log_thread();
+
+    for (size_t i(0), start(0), length(0); i < threadCount;
+         ++i, start += length, --extra)
+    {
+        length = (extra > 0) ? (partition + 1) : partition;
+
+        SequenceSubRange range
+            (values.begin() + start
+             , ((start + length) < values.size())
+             ? values.begin() + start + length : values.end());
+
+        typename ResultList::iterator iresult(result.begin() + start);
+
+        jobs.push_back
+            (std::async(std::launch::async, helper, range
+                        , str(boost::format("%s:%d/%d") % name
+                              % (i + 1) % threadCount)
+                        , start, count, iresult, callable, args...));
+    }
+
+    BOOST_FOREACH(auto &job, jobs) {
+        job.wait();
+    }
+
+    // stop thread logging
+    dbglog::log_thread(false);
+
+    return result;
+}
+
+} // namespace utility
+
+#endif // shared_utility_map_hpp_included_
