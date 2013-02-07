e1e8f33cf9e403f400dcda97c6d884456fdb2571
Václav Blažek <vaclav.blazek@citationtech.net>
2013-02-07 16:18:45 +0000

    shared/utility: added Boost.Program_options multivalue support
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@3083 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/detail/multivalue.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file detail/multivalue.hpp
+ * @author Vaclav Blazek <vaclav.blazek@citationtech.net>
+ *
+ * Config multivalue.
+ */
+
+#ifndef utility_detail_multivalues_hpp_included_
+#define utility_detail_multivalues_hpp_included_
+
+#include <string>
+#include <boost/program_options.hpp>
+
+/** This module adds support for C++ iomultivalue.
+ */
+namespace utility { namespace detail {
+namespace po = boost::program_options;
+
+template <typename T>
+const std::vector<T>
+inline get_multi_value(const detail::po::variables_map &map, const char *name
+                       , typename std::vector<T>::size_type size)
+{
+    const auto &v(map[name]);
+    if (v.empty()) {
+        throw detail::po::required_option(name);
+    }
+
+    const auto &value(v.as<std::vector<T> >());
+
+    if (value.size() != size) {
+        // TODO: replace "xxx" with size difference description
+        throw detail::po::validation_error
+            (detail::po::validation_error::invalid_option_value, "xxx", name);
+    }
+    return value;
+}
+
+} } // namespace utility::detail
+
+#endif // utility_detail_multivalues_hpp_included_
---
+++ utility/multivalue.hpp
@@ -0,0 +1,69 @@
+/**
+ * @file multivalue.hpp
+ * @author Vaclav Blazek <vaclav.blazek@citationtech.net>
+ *
+ * Config multivalue.
+ */
+
+#ifndef utility_multivalues_hpp_included_
+#define utility_multivalues_hpp_included_
+
+#include "detail/multivalue.hpp"
+
+/** This module adds support for Boost.Program_options multivalue.
+ */
+namespace utility {
+
+template <typename T>
+inline detail::po::value_semantic* multi_value();
+
+template <typename T, typename C>
+void process_multi_value(const detail::po::variables_map &map, const char *name
+                         , std::vector<C> &output, T C::*destination);
+
+
+template <typename T, typename U, typename C, typename D, typename O>
+void process_multi_value(const detail::po::variables_map &map, const char *name
+                         , std::vector<O> &output, T C::*owner
+                         , U D::*destination);
+
+// inline function implementation
+
+template <typename T>
+inline detail::po::value_semantic* multi_value()
+{
+    return detail::po::value<std::vector<T> >();
+}
+
+template <typename T, typename C>
+inline void process_multi_value(const detail::po::variables_map &map
+                                , const char *name
+                                , std::vector<C> &output, T C::*destination)
+{
+    const auto &input(detail::get_multi_value<T>(map, name, output.size()));
+
+    auto ioutput(output.begin());
+
+    for (const auto &value : input) {
+        ((*ioutput++).*destination) = value;
+    }
+}
+
+template <typename T, typename U, typename C, typename D, typename O>
+inline void process_multi_value(const detail::po::variables_map &map
+                                , const char *name
+                                , std::vector<O> &output, T C::*owner
+                                , U D::*destination)
+{
+    const auto &input(detail::get_multi_value<U>(map, name, output.size()));
+
+    auto ioutput(output.begin());
+
+    for (const auto &value : input) {
+        (((*ioutput++).*owner).*destination) = value;
+    }
+}
+
+} // namespace utility
+
+#endif // utility_multivalues_hpp_included_
