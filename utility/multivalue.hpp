1fb1b39c38c189a74e9aa5f48f14fce57a00ec95
Václav Blažek <vaclav.blazek@citationtech.net>
2013-02-12 19:19:27 +0000

    shared/utility: updated multivalue support
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@3161 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/multivalue.hpp
+++ utility/multivalue.hpp
@@ -35,6 +35,20 @@ inline detail::po::value_semantic* multi_value()
     return detail::po::value<std::vector<T> >();
 }
 
+template <typename T, typename C>
+inline void process_multi_value(const detail::po::variables_map &map
+                                , const char *name
+                                , std::vector<C> &output, T C::*destination)
+{
+    const auto &input(detail::get_multi_value<T>(map, name, output.size()));
+
+    auto ioutput(output.begin());
+
+    for (const auto &value : input) {
+        ((*ioutput++).*destination) = value;
+    }
+}
+
 template <typename T, typename C, typename Output>
 inline void process_multi_value(const detail::po::variables_map &map
                                 , const char *name
@@ -53,6 +67,22 @@ template <typename T, typename U, typename C, typename D, typename O>
 inline void process_multi_value(const detail::po::variables_map &map
                                 , const char *name
                                 , std::vector<O> &output, T C::*owner
+                                , U D::*destination)
+{
+    const auto &input(detail::get_multi_value<U>(map, name, output.size()));
+
+    auto ioutput(output.begin());
+
+    for (const auto &value : input) {
+        (((*ioutput++).*owner).*destination) = value;
+    }
+}
+
+template <typename T, typename U, typename C, typename D, typename O
+          , typename Output>
+inline void process_multi_value(const detail::po::variables_map &map
+                                , const char *name
+                                , Output &output, T C::*owner
                                 , U D::*destination)
 {
     const auto &input(detail::get_multi_value<U>(map, name, output.size()));
