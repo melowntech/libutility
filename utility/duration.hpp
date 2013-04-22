c1e30cf6f3d1cad2d3ef9e37dd592e54eb39b6df
Ladislav Horký <ladislav.horky@citationtech.net>
2013-04-22 08:03:14 +0000

    =Expanded time measurement.
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@4084 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/duration.hpp
+++ utility/duration.hpp
@@ -2,6 +2,7 @@
 #define utility_duration_hpp_included_
 
 #include <chrono>
+#include <boost/thread.hpp>
 
 namespace utility {
 
@@ -36,6 +37,49 @@ private:
 };
 
 typedef DurationMeter::duration_type Duration;
+
+
+//static class for counter registering .. NOT threadsafe
+class TimeMetrics {
+public:
+    typedef std::pair<DurationMeter, Duration> Stopwatch;
+    //registers counter upon the first call
+    static void startCounter(std::string name){
+        if(!watches_.get()){
+            watches_.reset(new std::map<std::string, Stopwatch>());
+        }
+        (*watches_)[name].first.update();
+    }
+    
+    static std::string stopCounterAndPrintTotal(std::string name){
+        if(!watches_.get()){
+            watches_.reset(new std::map<std::string, Stopwatch>());
+        }
+        if (watches_->find(name) == watches_->end()){
+            return "TimeMetrics: counter "+name+" not found!";
+        }else{
+            (*watches_)[name].second += (*watches_)[name].first.fromLast();
+            return "TimeMetrics: "+name+" total: "
+                    +boost::lexical_cast<std::string>(
+                        (*watches_)[name].second.count());
+        }
+    }
+private:
+    static boost::thread_specific_ptr<std::map<std::string, Stopwatch>> watches_;
+};
+
+#ifdef UTILITY_TIMERS
+    #define UTILITY_TIMEMETRICS_START_COUNTER(NAME)\
+        utility::TimeMetrics::startCounter(NAME)
+
+    #define UTILITY_TIMEMETRICS_STOP_COUNTER_AND_PRINT_TOTAL(NAME)\
+        LOG(info2) << utility::TimeMetrics::stopCounterAndPrintTotal(NAME);
+#else
+    #define UTILITY_TIMEMETRICS_START_COUNTER(NAME)          
+    #define UTILITY_TIMEMETRICS_STOP_COUNTER_AND_PRINT_TOTAL(NAME)
+#endif
+
+
 
 } // namespace utility
 
--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,6 +1,9 @@
 #include "map.hpp"
 #include "has_member.hpp"
+#include "duration.hpp"
 
 namespace utility {
     void placeholder() {}
+    boost::thread_specific_ptr<std::map<std::string, TimeMetrics::Stopwatch>> 
+            TimeMetrics::watches_;
 } // namespace utility
