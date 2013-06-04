bd80e9f0d1ca3732bc70868692d0bc16668ef6c2
Václav Blažek <vaclav.blazek@citationtech.net>
2013-06-04 12:44:25 +0000

    shared/utility: added uri and separated values parsing
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@4348 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/parse.hpp
@@ -0,0 +1,77 @@
+#ifndef utility_parse_hpp_included_
+#define utility_parse_hpp_included_
+
+#include <cstddef>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <limits>
+
+#include <boost/tokenizer.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/filesystem.hpp>
+
+namespace utility {
+
+struct LineRange {
+    LineRange(std::size_t from = 0
+              , std::size_t to = std::numeric_limits<std::size_t>::max())
+        : from(from), to(to)
+    {}
+
+    std::size_t from;
+    std::size_t to;
+};
+
+namespace separated_values {
+
+template <typename Row>
+std::vector<Row> parse(std::istream &is, const std::string &separator
+                       , const LineRange &range = LineRange())
+{
+    typedef boost::char_separator<char> Separator;
+    typedef boost::tokenizer<Separator> Tokenizer;
+    Separator sep(separator.c_str());
+    std::string line;
+    std::vector<std::string> values;
+
+    std::vector<Row> rows;
+    std::size_t index(0);
+    while (getline(is, line) && (index <= range.to)) {
+        // skip until from is reached
+        if (index++ < range.from) { continue; }
+        boost::algorithm::trim(line);
+        if (line.empty() || (line[0] == '#')) { continue; }
+
+        ++index;
+        Tokenizer tok(line, sep);
+        values.clear();
+        std::transform(tok.begin(), tok.end(), back_inserter(values)
+                       , [](const std::string &token) {
+                           return boost::algorithm::trim_copy(token);
+                       });
+
+        rows.emplace_back(values);
+    }
+
+    return rows;
+}
+
+template <typename Row>
+std::vector<Row> parse(const boost::filesystem::path &path
+                       , const std::string &separator
+                       , const LineRange &range = LineRange())
+{
+    std::ifstream f;
+    f.exceptions(std::ios::badbit | std::ios::failbit);
+    f.open(path.string(), std::ios_base::in);
+    f.exceptions(std::ios::badbit);
+
+    return parse<Row>(f, separator, range);
+}
+
+} // namespace separated_values
+
+} // namespace utility
+
+#endif // utility_parse_hpp_included_
---
+++ utility/uri.hpp
@@ -0,0 +1,124 @@
+#ifndef utility_uri_hpp_included_
+#define utility_uri_hpp_included_
+
+/*
+ * Uri parsing
+ *
+ * Based on code from https://github.com/CovenantEyes/uri-parser (BSD-licenced)
+ *
+ * Modified by Vaclav Blazek <vaclav.blazek@citationtech.net>
+ */
+
+#include <string>
+#include <cstdlib>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+
+namespace utility {
+
+struct Uri {
+    std::string protocol;
+    std::string user;
+    std::string password;
+    std::string host;
+    std::string path;
+    std::string search;
+    int port;
+};
+
+namespace detail {
+
+//--- Helper Functions --------------------------------------------------------
+inline std::string tailSlice(std::string &subject, const std::string &delimiter
+                             , bool keep_delim = false)
+{
+    // Chops off the delimiter and everything that follows
+    // (destructively) returns everything after the delimiter
+    auto delimiterLocation = subject.find(delimiter);
+
+    if (delimiterLocation == std::string::npos) {
+        return {};
+    }
+
+    auto start(keep_delim ? delimiterLocation
+               : delimiterLocation + delimiter.length());
+    auto end(subject.length() - start);
+    auto output(subject.substr(start, end));
+    subject = subject.substr(0, delimiterLocation);
+
+    return output;
+}
+
+inline std::string headSlice(std::string &subject
+                             , const std::string &delimiter)
+{
+    // Chops off the delimiter and everything that precedes (destructively)
+    // returns everthing before the delimeter
+    auto delimiterLocation(subject.find(delimiter));
+
+    if (delimiterLocation == std::string::npos) {
+        return {};
+    }
+
+    auto output(subject.substr(0, delimiterLocation));
+    subject = subject.substr
+        (delimiterLocation + delimiter.length()
+         , subject.length() - (delimiterLocation + delimiter.length()));
+    return output;
+}
+
+
+//--- Extractors -----------------------
+inline int extractPort(std::string &hostport) {
+    try {
+        return boost::lexical_cast<int>(tailSlice(hostport, ":"));
+    } catch (const boost::bad_lexical_cast&) {
+        return -1;
+    }
+}
+
+inline std::string extractPath(std::string &in) {
+    return tailSlice(in, "/", true);
+}
+
+inline std::string extractProtocol(std::string &in) {
+    return headSlice(in, "://");
+}
+
+inline std::string extractSearch(std::string &in) {
+    return tailSlice(in, "?");
+}
+
+inline std::string extractPassword(std::string &userpass) {
+    return tailSlice(userpass, ":");
+}
+
+inline std::string extractUserpass(std::string &in) {
+    return headSlice(in, "@");
+}
+
+} // namespace detail
+
+
+//--- Public Interface --------------------------------------------------------
+inline Uri parseUri(std::string in) {
+    Uri ret;
+    ret.port = -1;
+
+    ret.protocol
+        = boost::algorithm::to_lower_copy(detail::extractProtocol(in));
+    ret.search = detail::extractSearch(in);
+    ret.path = detail::extractPath(in);
+    std::string userpass = detail::extractUserpass(in);
+    ret.password = detail::extractPassword(userpass);
+    ret.user = userpass;
+    ret.port = detail::extractPort(in);
+    ret.host = boost::algorithm::to_lower_copy(in);
+
+    return ret;
+}
+
+} // namespace utility
+
+#endif // utility_uri_hpp_included_
