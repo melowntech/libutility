9d2f53b680549404ba5a4a916ff9226b27cec724
Václav Blažek <vaclav.blazek@citationtech.net>
2013-08-26 16:27:18 +0000

    shared/utility: added Simple Runnable type
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@5464 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/runnable.hpp
+++ utility/runnable.hpp
@@ -1,6 +1,7 @@
 #ifndef shared_utility_runnable_hpp_included_
 #define shared_utility_runnable_hpp_included_
 
+#include <atomic>
 #include <boost/noncopyable.hpp>
 
 namespace utility {
@@ -12,6 +13,19 @@ public:
     virtual void stop() = 0;
 
     inline operator bool() { return isRunning(); }
+
+    class Simple;
+};
+
+class Runnable::Simple : public Runnable
+{
+public:
+    Simple() : running_(true) {}
+    bool isRunning() { return running_; }
+    void stop() { running_ = false; }
+
+private:
+    std::atomic_bool running_;
 };
 
 } // namespace utility
