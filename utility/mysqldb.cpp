0143a5b108868a844c0dc986b20f42f2fc8be635
Jakub Červený <jakub.cerveny@ext.citationtech.net>
2013-09-26 07:51:06 +0000

    utility: added mysqldb module
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@5997 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/mysqldb.cpp
@@ -0,0 +1,94 @@
+#include <boost/filesystem.hpp>
+
+#include "utility/config.hpp"
+#include "dbglog/dbglog.hpp"
+
+#include "db.hpp"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+void Db::Parameters::configuration(const std::string &section
+                                   , po::options_description &config)
+{
+    config.add_options()
+        ((section + "database").c_str()
+         , po::value(&database)
+         , "Name of database to select upon connection.")
+        ((section + "host").c_str()
+         , po::value(&host)->default_value(host)
+         , ("Specifies the IPC method and parameters for "
+            "contacting the server."))
+        ((section + "user").c_str()
+         , po::value(&user)->default_value(user)
+         , ("User name to log in under, or \"\" to use the user name this "
+            "program is running under."))
+        ((section + "password").c_str()
+         , po::value(&password)
+         , "Password to use when logging in.")
+        ((section + "port").c_str()
+         , po::value(&port)->default_value(port)
+         , ("TCP port number database server is listening on, "
+            "or 0 to use default value; note that you may also give this as "
+            "part of the server parameter."))
+        ;
+}
+
+Db::Db(const Parameters &params)
+    : params_(params)
+    , conn_()
+{
+    conn_.set_option(new mysqlpp::MultiStatementsOption(true));
+    conn_.connect(params_.database.c_str()
+                  , (params_.host.empty() ? nullptr : params_.host.c_str())
+                  , (params_.user.empty() ? nullptr : params_.user.c_str())
+                  , params_.password.c_str()
+                  , params_.port);
+
+    LOG(info2) << "Connected to database <" << params_.database
+               << "> at <" << params_.host << "> as <"
+               << params_.user << ">.";
+
+    conn_.query("SET NAMES utf8").exec();
+    conn_.query("SET sql_mode='STRICT_TRANS_TABLES'").exec();
+    conn_.query("SET AUTOCOMMIT=0").exec();
+}
+
+Db::~Db()
+{
+    LOG(info2) << "Disconnected from database <" << params_.database
+               << "> at <" << params_.host << ">.";
+}
+
+void Db::prepareConfig(po::options_description &config
+                       , const std::string &rootRef)
+{
+    config.add_options()
+        ("dbconf", po::value<fs::path>()->required()
+         , ("Path to DB configuration (relative path is appended to "
+            + rootRef + ").").c_str())
+        ;
+}
+
+void Db::configHelp(std::ostream &out)
+{
+    Parameters dbconf;
+    po::options_description config("dbconf configuration file");
+    dbconf.configuration("db.", config);
+    out << config;
+}
+
+Db::Parameters Db::fromConfig(const po::variables_map &vars, fs::path root
+                              , fs::path *outPath)
+{
+    // read db configuration
+    Db::Parameters dbconf;
+    fs::path path(vars["dbconf"].as<fs::path>());
+    if (!path.is_absolute()) {
+        path = absolute(path, root);
+    }
+
+    if (outPath) { *outPath = path; }
+    utility::readConfig(path, dbconf, "db.");
+    return dbconf;
+}
---
+++ utility/mysqldb.hpp
@@ -0,0 +1,144 @@
+#ifndef fetcher_db_hpp_included_
+#define fetcher_db_hpp_included_
+
+#include <ctime>
+#include <string>
+#include <type_traits>
+#include <stdexcept>
+#include <exception>
+#include <ostream>
+
+#include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
+#include <boost/format.hpp>
+#include <boost/program_options.hpp>
+
+#include <mysql++.h>
+
+#include "dbglog/dbglog.hpp"
+
+class Db : boost::noncopyable {
+public:
+    typedef boost::optional<Db> handle;
+    typedef mysqlpp::Connection Connection;
+    typedef mysqlpp::Query Query;
+    typedef mysqlpp::SimpleResult SimpleResult;
+    typedef mysqlpp::StoreQueryResult StoreQueryResult;
+
+    struct Parameters {
+        std::string database;
+        std::string host;
+        std::string user;
+        std::string password;
+        unsigned int port;
+
+        Parameters() : port(0) {}
+
+        void configuration(const std::string &section
+                           , boost::program_options::options_description
+                           &config);
+
+        void configure(const boost::program_options::variables_map &) {}
+
+        template <typename E, typename T>
+        std::basic_ostream<E, T>& dump(std::basic_ostream<E, T> &os
+                                       , const std::string &section = "")
+            const;
+    };
+
+    struct QueryError : std::runtime_error {
+        QueryError(const mysqlpp::Exception &e, const std::string &query);
+        std::exception_ptr exc;
+    };
+
+    /** Create DB connection. */
+    Db(const Parameters &params);
+
+    ~Db();
+
+    inline const Parameters& params() const { return params_; }
+
+    /** Connect to database if not connected yet and return
+     * underlying database connection. */
+    inline Connection& conn() { return conn_; }
+
+    inline Query query() { return conn_.query(); }
+
+    template <typename T>
+    inline Query query(const T &t) { return conn_.query(t); }
+
+    SimpleResult execute(Query &query);
+
+    StoreQueryResult store(Query &query);
+
+    static void prepareConfig(boost::program_options::options_description
+                              &config
+                              , const std::string &rootRef = "cwd");
+
+    static Parameters
+    fromConfig(const boost::program_options::variables_map &vars
+               , boost::filesystem::path root = boost::filesystem::path()
+               , boost::filesystem::path *path = nullptr);
+
+    static void configHelp(std::ostream &out);
+
+private:
+    const Parameters params_;
+    Connection conn_;
+};
+
+template <typename T>
+boost::optional<T> optional(const mysqlpp::String &s)
+{
+    if (s.is_null()) {
+        return boost::none;
+    }
+    return T(s);
+}
+
+// inline method implementation
+
+inline Db::QueryError::QueryError(const mysqlpp::Exception &e
+                                  , const std::string &query)
+    : std::runtime_error
+      (str(boost::format("Query execution failed: <%s>; query: %s")
+           % e.what() % query))
+    , exc(std::make_exception_ptr(e))
+{}
+
+inline Db::SimpleResult Db::execute(Query &query) {
+    try {
+        LOG(info1) << "Executing query: " << query;
+        return query.execute();
+    } catch (const mysqlpp::Exception &e) {
+        QueryError qe(e, query.str());
+        LOG(err2) << qe.what();
+        throw qe;
+    }
+}
+
+inline Db::StoreQueryResult Db::store(Query &query) {
+    try {
+        LOG(info1) << "Executing query: " << query;
+        return query.store();
+    } catch (const mysqlpp::Exception &e) {
+        QueryError qe(e, query.str());
+        LOG(err2) << qe.what();
+        throw qe;
+    }
+}
+
+template <typename E, typename T>
+std::basic_ostream<E, T>&
+Db::Parameters::dump(std::basic_ostream<E, T> &os, const std::string &section)
+    const
+{
+    // NB: do not print password!
+    return os << section << "database = " << database << '\n'
+              << section << "host = " << host << '\n'
+              << section << "user = " << user << '\n'
+              << section << "port = " << port << '\n';
+}
+
+#endif // fetcher_db_hpp_included_
+
