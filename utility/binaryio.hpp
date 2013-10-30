0d1347584e874e045b0529bf46641e45e768abb9
Jakub Červený <jakub.cerveny@ext.citationtech.net>
2013-10-30 15:11:27 +0000

    utility: moved binary io helpers from cvision/matio.cpp to separate header
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@6530 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/binaryio.hpp
@@ -0,0 +1,50 @@
+#ifndef utility_binaryio_hpp_included_
+#define utility_binaryio_hpp_included_
+
+namespace utility { namespace binaryio {
+
+inline void write(std::ostream &os, const char *v, size_t count) {
+    os.write(v, count);
+}
+
+template <typename T>
+void write(std::ostream &os, const T *v, size_t count)
+{
+    os.write(reinterpret_cast<const char*>(v), count * sizeof(T));
+}
+
+template<typename T, int size>
+void write(std::ostream &os, const T(&v)[size]) {
+    os.write(reinterpret_cast<const char*>(v), size * sizeof(T));
+}
+
+template <typename T>
+void write(std::ostream &os, const T &v)
+{
+    os.write(reinterpret_cast<const char*>(&v), sizeof(T));
+}
+
+inline void read(std::istream &os, char *v, size_t count) {
+    os.read(v, count);
+}
+
+template <typename T>
+void read(std::istream &os, T *v, size_t count)
+{
+    os.read(reinterpret_cast<char*>(v), count * sizeof(T));
+}
+
+template<typename T, int size>
+void read(std::istream &os, T(&v)[size]) {
+    os.read(reinterpret_cast<char*>(v), size * sizeof(T));
+}
+
+template <typename T>
+void read(std::istream &os, T &v)
+{
+    os.read(reinterpret_cast<char*>(&v), sizeof(T));
+}
+
+} } // namespace utility::binaryio
+
+#endif // utility_binaryio_hpp_included_
