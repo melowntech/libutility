d22491ab6165243d45a9be60e59c2c8e67a10a0d
Václav Blažek <vaclav.blazek@citationtech.net>
2013-11-15 08:41:33 +0000

    shared/utility: added path test
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@6952 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/test/path.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+#include "utility/path.hpp"
+
+#include "dbglog/dbglog.hpp"
+
+BOOST_AUTO_TEST_CASE(utility_path_extension)
+{
+    BOOST_TEST_MESSAGE("* Testing utility/path.");
+
+    namespace fs = boost::filesystem;
+
+    fs::path base("/usr/local/bin/script");
+    auto exe1(utility::addExtension(base, ".exe"));
+    auto exe2(utility::replaceOrAddExtension(base, ".exe"));
+    auto sh1(utility::replaceOrAddExtension(exe1, ".sh"));
+
+    LOG(info4) << "base: " << base;
+    LOG(info4) << "added .exe: " << exe1;
+    LOG(info4) << "added/replaced .exe: " << exe2;
+    LOG(info4) << "added/replaced .exe -> .sh: " << sh1;
+}
