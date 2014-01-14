c281ac70790562dcaf77ccca05a8e131ae9fd6f9
Václav Blažek <vaclav.blazek@citationtech.net>
2014-01-14 13:20:44 +0000

    shared/utility: added duration formatting
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@8322 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/time.hpp
@@ -0,0 +1,31 @@
+#ifndef utility_time_hpp_included_
+#define utility_time_hpp_included_
+
+#include <sstream>
+#include <iomanip>
+#include <chrono>
+#include "steady-clock.hpp"
+
+namespace utility {
+
+template <typename TimeDuration>
+std::string formatDuration(const TimeDuration &d)
+{
+    using namespace std::chrono;
+
+    std::ostringstream os;
+    os << std::setw(2) << std::setfill('0')
+       << duration_cast<hours>(d).count() << ':'
+       << std::setw(2) << std::setfill('0')
+       << (duration_cast<minutes>(d).count() % 60) << ':'
+       << std::setw(2) << std::setfill('0')
+       << (duration_cast<seconds>(d).count() % 60) << '.'
+       << std::setw(6) << std::setfill('0')
+       << (duration_cast<microseconds>(d).count() % 1000000);
+
+    return os.str();
+}
+
+} // namespace utility
+
+#endif // utility_time_hpp_included_
