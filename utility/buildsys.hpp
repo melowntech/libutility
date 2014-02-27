148651c544784cdccc6dd2b208c879b935f80af7
Václav Blažek <vaclav.blazek@citationtech.net>
2014-02-27 15:33:59 +0000

    shared/utility: updated layout
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@8923 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/buildsys.hpp
+++ utility/buildsys.hpp
@@ -8,27 +8,30 @@
 #ifndef utility_buildsys_hpp_included_
 #define utility_buildsys_hpp_included_
 
+#include "boost/filesystem/path.hpp"
+
 namespace utility { namespace buildsys {
 
+/** Updates path to be under installation prefix.
+ */
+boost::filesystem::path installPath(const boost::filesystem::path &p);
+
+// Fix missing macros
 #ifndef BUILD_TARGET_NAME
 #    define BUILD_TARGET_NAME "unknown"
 #endif
-constexpr char TargetName[] = BUILD_TARGET_NAME;
 
 #ifndef BUILD_TARGET_VERSION
 #    define BUILD_TARGET_VERSION "unknown"
 #endif
-constexpr char TargetVersion[] = BUILD_TARGET_VERSION;
 
 #ifndef BUILDSYS_INSTALL_PREFIX
 #    define BUILDSYS_INSTALL_PREFIX "/usr/local"
 #endif
-constexpr char InstallPrefix[] = BUILDSYS_INSTALL_PREFIX;
 
 #ifndef BUILDSYS_HOSTNAME
 #    define  BUILDSYS_HOSTNAME "unknown"
 #endif
-constexpr char Hostname[] = BUILDSYS_HOSTNAME;
 
 #ifdef BUILDSYS_CUSTOMER_BUILD
 #    ifndef BUILDSYS_CUSTOMER
@@ -37,7 +40,35 @@ constexpr char Hostname[] = BUILDSYS_HOSTNAME;
 #else
 #    define BUILDSYS_CUSTOMER "none"
 #endif
+
+/** Target name.
+ */
+constexpr char TargetName[] = BUILD_TARGET_NAME;
+
+/** Target version.
+ */
+constexpr char TargetVersion[] = BUILD_TARGET_VERSION;
+
+/** Target installation prefix.
+ */
+constexpr char InstallPrefix[] = BUILDSYS_INSTALL_PREFIX;
+
+/** Hostname this build is being compiled at.
+ */
+constexpr char Hostname[] = BUILDSYS_HOSTNAME;
+
+/** Customer this build is compiled for.
+ */
 constexpr char Customer[] = BUILDSYS_CUSTOMER;
+
+
+
+// inline stuff
+
+inline boost::filesystem::path installPath(const boost::filesystem::path &p)
+{
+    return InstallPrefix / p;
+}
 
 } } // namespace utility::buildsys
 
