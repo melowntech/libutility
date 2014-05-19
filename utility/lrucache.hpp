379698f7bb028cafc0b537461b6bbd84e74d3093
Václav Blažek <vaclav.blazek@citationtech.net>
2014-05-19 19:18:13 +0000

    shared/utility: updated lru cache
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@10017 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/lrucache.hpp
+++ utility/lrucache.hpp
@@ -9,46 +9,79 @@
 
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/identity.hpp>
 #include <boost/multi_index/member.hpp>
+
+#include "dbglog/dbglog.hpp"
+#include "utility/streams.hpp"
 
 namespace utility {
 
+/** Value traits for LruCache.
+ *
+ * Should contain 2 static functions:
+ *     key_ key(const Value &v)
+ *     cost_type cost(const Value &v)
+ *
+ */
 template <typename Value> struct LruCacheTraits;
+
+namespace detail {
 
 template <typename T> inline const T& reference() {
     return *static_cast<const T*>(nullptr);
 }
 
-template <typename Value>
+} // namespace detail
+
+/** LRU Cache for arbitrary values.
+ *  Values are held by share pointer.
+ *  There must be LruCacheTraits specialization for Value:
+ *      template <typename>
+ *      struct LruCacheTraits<Value> {
+ *          static key_type key(const Value &);
+ *          static cost_type cost(const Value &);
+ *      }
+ *
+ *  All operations on cache are thread safe.
+ */
+template <typename Value, typename TraitsType = LruCacheTraits<Value> >
 class LruCache : boost::noncopyable {
 public:
+    typedef TraitsType Traits;
     typedef std::shared_ptr<Value> value_pointer;
 
     /** Determine key type via traits
      */
     typedef typename std::remove_reference
-    <decltype(LruCacheTraits<Value>::key(reference<Value>()))>
+    <decltype(Traits::key(detail::reference<Value>()))>
     ::type key_type;
 
     /** Determine cost type via traits
      */
     typedef typename std::remove_reference
-    <decltype(LruCacheTraits<Value>::cost(reference<Value>()))>
+    <decltype(Traits::cost(detail::reference<Value>()))>
     ::type cost_type;
 
+    /** Create cache.
+     */
     LruCache() : priorityGenerator_() {}
 
-    void insert(const value_pointer &v);
+    /** Insert new element to cache. Returns false on conflict.
+     */
+    bool insert(const value_pointer &v);
 
+    /** Returns element under key or nullptr if not found.
+     *  Element is moved to to head of priority queue.
+     */
     value_pointer get(const key_type &key);
 
+    /** Removes as many as needed elements from priority queue tail until total
+     * cost of all items in the cache is not greated than provided limit.
+     */
     std::size_t trim(cost_type limit);
 
 private:
     typedef std::uint64_t priority_type;
-
-    typedef LruCacheTraits<Value> Traits;
 
     /** Cache entry.
      */
@@ -58,7 +91,8 @@ private:
         priority_type priority;
 
         template <typename T, typename E>
-        std::basic_ostream<T,E>& dump(std::basic_ostream<T,E> &os) const
+        std::basic_ostream<T,E>& dump(std::basic_ostream<T,E> &os
+                                      , const std::string&) const
         {
             return os << "value=" << value.get() << ", key=" << key
                       << ", priority=" << priority;
@@ -93,19 +127,25 @@ private:
     std::mutex mutex_;
 };
 
-template <typename Value>
-void LruCache<Value>::insert(const value_pointer &v)
+template <typename Value, typename TraitsType>
+bool LruCache<Value, TraitsType>::insert(const value_pointer &v)
 {
     std::unique_lock<std::mutex> lock(mutex_);
 
-    container_.insert(Entry{v, Traits::key(*v), priorityGenerator_});
+    if (!container_.insert(Entry{v, Traits::key(*v), priorityGenerator_})
+        .second)
+    {
+        // key conflict!
+        return false;
+    }
     ++priorityGenerator_;
     totalCost_ += Traits::cost(*v);
+    return true;
 }
 
-template <typename Value>
-typename LruCache<Value>::value_pointer
-LruCache<Value>::get(const key_type &key)
+template <typename Value, typename TraitsType>
+typename LruCache<Value, TraitsType>::value_pointer
+LruCache<Value, TraitsType>::get(const key_type &key)
 {
     std::unique_lock<std::mutex> lock(mutex_);
 
@@ -119,13 +159,10 @@ LruCache<Value>::get(const key_type &key)
         return {};
     }
 
-    std::cout << "found entry ";
-    fkeys->dump(std::cout);
-    std::cout << std::endl;
+    LOG(debug) << "lru-cache: found entry " << utility::dump(*fkeys);
 
-    // FIXME: this may throw when entry cannot be modified (it removes the
-    // entry completely)
-    container_.modify
+    // modify entry (this may fail!)
+    keys.modify
         (fkeys, [&](Entry &e) { e.priority = priorityGenerator_; });
 
     ++priorityGenerator_;
@@ -133,8 +170,8 @@ LruCache<Value>::get(const key_type &key)
     return fkeys->value;
 }
 
-template <typename Value>
-std::size_t LruCache<Value>::trim(cost_type limit)
+template <typename Value, typename TraitsType>
+std::size_t LruCache<Value, TraitsType>::trim(cost_type limit)
 {
     std::unique_lock<std::mutex> lock(mutex_);
 
@@ -146,11 +183,11 @@ std::size_t LruCache<Value>::trim(cost_type limit)
              , epriorities(priorities.end())
              ; (totalCost_ > limit) && (ipriorities != epriorities); )
     {
-        std::cout << "Removing: ";
-        ipriorities->dump(std::cout);
-        std::cout << " because total " << totalCost_ << " > "
-                  << limit << std::endl;
+        LOG(debug) << "lru-cache: removing " << utility::dump(*ipriorities)
+                   << " because total " << totalCost_ << " > "
+                   << limit;
 
+        // remove entry and update total cost
         auto cost(Traits::cost(*ipriorities->value));
         ipriorities = priorities.erase(ipriorities);
         totalCost_ -= cost;
