8cd840e4a5de7ee9d4a2c8f0ba029aeb86ed9668
Václav Blažek <vaclav.blazek@citationtech.net>
2015-02-26 15:07:18 +0000

    shared/utility: added magic support (libmagic, not magic :) )
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@12588 b7e6147c-0f76-4c84-a09d-19069afaf10c

---
+++ utility/magic.cpp
@@ -0,0 +1,54 @@
+#include <type_traits>
+
+#include <magic.h>
+
+#include "dbglog/dbglog.hpp"
+
+#include "magic.hpp"
+
+namespace utility {
+
+typedef std::remove_pointer< ::magic_t>::type MagicCookie;
+
+Magic::Magic()
+    : magic_(magic_open(MAGIC_MIME_TYPE), [] (magic_t m) {
+            if (m) ::magic_close(m);
+        })
+{
+    auto cookie(std::static_pointer_cast<MagicCookie>(magic_).get());
+    if (::magic_load(cookie, nullptr) == -1) {
+        LOGTHROW(err1, MagicError)
+            << "Cannot load magic database: <"
+            << ::magic_error(cookie) << ">.";
+    }
+}
+
+std::string Magic::mime(const void *buffer, std::size_t length) const
+{
+    auto cookie(std::static_pointer_cast<MagicCookie>(magic_).get());
+    auto res(magic_buffer(cookie, buffer, length));
+
+    if (!res) {
+        LOGTHROW(err1, MagicError)
+            << "Cannot determine magic of given buffer: <"
+            << ::magic_error(cookie) << ">.";
+    }
+
+    return res;
+}
+
+std::string Magic::mime(const boost::filesystem::path &path) const
+{
+    auto cookie(std::static_pointer_cast<MagicCookie>(magic_).get());
+    auto res(magic_file(cookie, path.string().c_str()));
+
+    if (!res) {
+        LOGTHROW(err1, MagicError)
+            << "Cannot determine magic of given buffer: <"
+            << ::magic_error(cookie) << ">.";
+    }
+
+    return res;
+}
+
+} // namespace utility
---
+++ utility/magic.hpp
@@ -0,0 +1,31 @@
+#ifndef utility_magic_hpp_included_
+#define utility_magic_hpp_included_
+
+#include <cstddef>
+#include <stdexcept>
+#include <memory>
+#include <string>
+
+#include <boost/filesystem/path.hpp>
+
+namespace utility {
+
+struct MagicError : std::runtime_error {
+    MagicError(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+class Magic {
+public:
+    Magic();
+
+    std::string mime(const void *buffer, std::size_t length) const;
+
+    std::string mime(const boost::filesystem::path &path) const;
+
+private:
+    std::shared_ptr<void> magic_;
+};
+
+} // namespace utility
+
+#endif // utility_magic_hpp_included_
