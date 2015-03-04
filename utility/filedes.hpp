39dd9aaca3ad4794071715867bb76e859fae2cb5
Václav Blažek <vaclav.blazek@citationtech.net>
2015-03-04 15:56:07 +0000

    shared/utility: added (optional) associated path to Filedes
    git-svn-id: https://scm.citationtech.net/trunk/shared/utility/utility@12700 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- utility/filedes.hpp
+++ utility/filedes.hpp
@@ -2,33 +2,54 @@
 #define utility_filesed_hpp_included_
 
 #include <utility>
+#include <boost/filesystem/path.hpp>
 
 namespace utility {
 
+/** Simple wrapper around file descriptor.
+ */
 class Filedes {
 public:
     Filedes() : fd_(-1) {}
-    Filedes(int fd) : fd_(fd) {}
-    Filedes(Filedes &&other) : fd_(other.fd_) { other.fd_ = -1; }
+    explicit Filedes(int fd) : fd_(fd) {}
+
+    Filedes(int fd, const boost::filesystem::path &path)
+        : fd_(fd), path_(path)
+    {}
+
+    Filedes(Filedes &&other)
+        : fd_(other.fd_), path_(std::move(other.path_))
+    {
+        other.fd_ = -1;
+    }
 
     Filedes& operator=(Filedes &&other) {
         std::swap(fd_, other.fd_);
+        path_ = std::move(other.path_);
         return *this;
     }
 
     ~Filedes();
-    // ~Filedes() { if (fd_ >= 0) { TEMP_FAILURE_RETRY(::close(fd_)); } }
 
     int get() const { return fd_; }
+    const boost::filesystem::path& path() const { return path_; }
 
     operator int() const { return get(); }
+    operator bool() const { return fd_ >= 0; }
 
-    int release() { auto fd(fd_); fd_ = -1; return fd; }
+    int release() {
+        auto fd(fd_);
+        fd_ = -1;
+        path_.clear();
+        return fd;
+    }
 
-private:
     Filedes(const Filedes&) = delete;
     Filedes& operator=(const Filedes&) = delete;
+
+private:
     int fd_;
+    boost::filesystem::path path_;
 };
 
 } // namespace utility
