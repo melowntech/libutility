c39a2e3401586ba447b2cf9951de5ad874aff6b8
Vaclav Blazek <vaclav.blazek@citationtech.net>
2016-03-24 13:51:45 +0100

    added makeError() function

--- utility/raise.hpp
+++ utility/raise.hpp
@@ -17,6 +17,12 @@ inline std::string formatError(const std::string &message, Args &&...args)
 }
 
 template <typename Exception, typename ...Args>
+inline Exception makeError(const std::string &message, Args &&...args)
+{
+    return Exception(formatError(message, std::forward<Args>(args)...));
+}
+
+template <typename Exception, typename ...Args>
 inline void raise(const std::string &message, Args &&...args)
 {
     throw Exception(formatError(message, std::forward<Args>(args)...));
