855ac22e7f9623348dbf53ccca05b9d22327d3e0
Vaclav Blazek <vaclav.blazek@citationtech.net>
2019-03-12 19:01:37 +0100

    Addes simple gzipping stream wrapper

---
+++ utility/gzipper.hpp
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2019 Melown Technologies SE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * *  Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef utility_gzipper_hpp_included_
+#define utility_gzipper_hpp_included_
+
+#include <exception>
+
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/restrict.hpp>
+
+namespace utility {
+
+/** Simple wrapper around external ostream that adds gzipping.
+ *  Should be used as a temporary when passing down a real stream
+ */
+class Gzipper {
+public:
+    Gzipper(std::ostream &os) {
+        gzipped_.push(boost::iostreams::gzip_compressor
+                     (boost::iostreams::gzip_params(9), 1 << 16));
+        gzipped_.push(os);
+    }
+
+    ~Gzipper() {
+        if (!std::uncaught_exception()) {
+            // no exception -> flush
+            gzipped_.flush();
+        }
+    }
+
+    operator std::ostream&() const { return gzipped_; }
+
+private:
+    mutable boost::iostreams::filtering_ostream gzipped_;
+};
+
+} // namespace utility
+
+#endif // utility_gzipper_hpp_included_
