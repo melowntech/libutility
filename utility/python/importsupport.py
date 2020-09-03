ec3d96f2ef11f1e54f39e65e6173f76037c81649
Vaclav Blazek <vaclav.blazek@citationtech.net>
2020-09-03 19:12:43 +0200

    python: file_from_archive helper function

--- utility/python/importsupport.py
+++ utility/python/importsupport.py
@@ -30,3 +30,26 @@ def import_extension(loader, name, file, modulename):
 
     module.__file__ = os.path.dirname(file)
     return module
+
+
+def file_from_archive(loader, name, file, filename):
+    import os
+    import os.path
+    root = os.path.dirname(file)
+
+    if hasattr(loader, "archive"):
+        archive = os.path.join(loader.prefix, loader.archive)
+        zip_filename = os.path.join(os.path.relpath(root, archive)
+                                    , filename)
+
+        import zipfile
+        z = zipfile.ZipFile(archive)
+
+        from melown import utility
+        mf = utility.memoryFile(name, utility.MemoryFileFlag.closeOnExec)
+
+        buf = z.open(zip_filename).read();
+        os.write(mf.fileno(), buf)
+        return mf.path(), mf
+
+    return os.path.join(root, filename), None
