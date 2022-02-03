bc14c3b357f17fe48bcc05fc48cb27058a5fafd4
Jan Zapletal <zaja@bullseye.localdomain>
2022-02-03 13:22:10 +0100

    ENH: c++17 support

---
+++ utility/uncaught-exception.cpp
@@ -0,0 +1,15 @@
+#include "uncaught-exception.hpp"
+
+namespace utility {
+
+bool uncaught_exception() noexcept
+{
+#if __cplusplus >= 201703L
+    return std::uncaught_exceptions();
+#else
+    return std::uncaught_expcetion();
+#endif
+}
+
+} // utility
+
