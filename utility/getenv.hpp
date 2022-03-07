1079b4cea6a697ad1fd2860fd4de4d01d0b0188e
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2022-03-07 12:21:54 +0100

    fix pragma warning

--- utility/getenv.hpp
+++ utility/getenv.hpp
@@ -3,11 +3,6 @@
 
 #include "env.hpp"
 
-#ifndef _CRT_STRINGIZE
-#define _CRT_STRINGIZE_(x) #x
-#define _CRT_STRINGIZE(x) _CRT_STRINGIZE_(x)
-#endif
-
-#pragma message(__FILE__ "(" _CRT_STRINGIZE(__LINE__) ")" ": #include \"utility/getenv.hpp\" is deprecated, include \"env.hpp\" instead")
+#pragma message("warning : #include \"utility/getenv.hpp\" is deprecated, include \"env.hpp\" instead")
 
 #endif
